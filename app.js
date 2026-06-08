5be4a14057b70a4e81439952104e1b8fc33fc5f0
NITHIN-01-Reddy <Nithinreddy.S.D@gmail.com>
2026-06-08 07:58:47 +0530

    added simple paragraph

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('"<h1>Welcome to Nithin DevOps CI/CD Project</h1>"');
+  res.send('"<h1>Welcome to  DevOps CI/CD Project</h1>"<br>"<p>This is a simple Express application running on Node.js.</p>"');
 });
 
 app.listen(3000, () => {
